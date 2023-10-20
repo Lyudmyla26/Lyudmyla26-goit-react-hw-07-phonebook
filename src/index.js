91b8300013e1b442bd5b1c7b65fb2676066a51e6
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-10-20 17:16:00 +0300

    п

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
 
